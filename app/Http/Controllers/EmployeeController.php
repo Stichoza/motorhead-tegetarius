79c5537c20dcafa3bf785a811f8b2d5af73a8a50
Levan Velijanashvili <stichoza@gmail.com>
2015-12-30 15:40:40 +0400

    Some docblocks

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -5,17 +5,32 @@ namespace App\Http\Controllers;
 use App\Models\Employee;
 
 class EmployeeController extends Controller {
-	
+
+	/**
+	 * Main page for employees
+	 *
+	 * @return \Illuminate\View\View
+     */
 	public function index()
 	{
 		return view('index');
 	}
 
+	/**
+	 * Employee JSON
+	 *
+	 * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
 	public function json()
 	{
 		return Employee::all();
 	}
 
+	/**
+	 * Employee creation route
+	 *
+	 * @return \Illuminate\View\View
+     */
 	public function create()
 	{
 		return view('create');
