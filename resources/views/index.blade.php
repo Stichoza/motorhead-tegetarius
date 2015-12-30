ff9e720eeb68576a6405a219a1d71b5c42209e83
Levan Velijanashvili <stichoza@gmail.com>
2015-12-30 15:43:19 +0400

    Fix route

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-	<table class="list" data-toggle="table" data-url="{{ route('employee.index') }}" data-method="get" data-page-list="[5, 10, 50, 100, 500]" data-pagination="true" data-search="true" data-sort-name="name" data-select-item-name="selectListItem" data-id-field="id" data-buttons-align="right" data-toolbar-align="left" data-show-refresh="true" data-search-time-out="100" data-striped="true" data-show-columns="true" data-sort-order="asc" data-toolbar=".listToolbar">
+	<table class="list" data-toggle="table" data-url="{{ route('employee.json') }}" data-method="get" data-page-list="[5, 10, 50, 100, 500]" data-pagination="true" data-search="true" data-sort-name="name" data-select-item-name="selectListItem" data-id-field="id" data-buttons-align="right" data-toolbar-align="left" data-show-refresh="true" data-search-time-out="100" data-striped="true" data-show-columns="true" data-sort-order="asc" data-toolbar=".listToolbar">
 		<thead>
 			<tr>
 				<th data-click-to-select="true" data-sortable="true" data-field="name">სახელი, გვარი</th>
