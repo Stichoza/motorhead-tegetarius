456fd51ebc82fd7024bee152409be9a29cad16c8
Levan Velijanashvili <stichoza@gmail.com>
2015-12-30 06:17:05 +0400

    Create employee table

---
+++ database/migrations/2015_12_30_020734_create_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('sex', 6);
+            $table->string('position');
+            $table->decimal('salary', 15, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('employees');
+    }
+}
