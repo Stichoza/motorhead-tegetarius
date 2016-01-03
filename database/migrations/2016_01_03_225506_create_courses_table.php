f9a7dd5530e0a9ca2e33072f953c82fcb41ca827
Levan Velijanashvili <stichoza@gmail.com>
2016-01-04 03:07:36 +0400

    Migrations for additional tables

---
+++ database/migrations/2016_01_03_225506_create_courses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('employee_id')
+                ->unsigned();
+            $table->foreign('employee_id')
+                ->references('id')
+                ->on('employees')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('title');
+            $table->date('start');
+            $table->date('end');
+            $table->text('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('courses');
+    }
+}
---
+++ database/migrations/2016_01_03_230240_create_addresses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('employee_id')
+                ->unsigned();
+            $table->foreign('employee_id')
+                ->references('id')
+                ->on('employees')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('city');
+            $table->string('street');
+            $table->string('number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('addresses');
+    }
+}
