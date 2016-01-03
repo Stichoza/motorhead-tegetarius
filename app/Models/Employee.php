c3416ea10dec5300fa6073d807c1394d4f4ebf26
Levan Velijanashvili <stichoza@gmail.com>
2016-01-04 03:16:02 +0400

    Model relations

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,20 @@ class Employee extends Model {
 
     protected $fillable = ['name', 'sex', 'position', 'salary'];
 
+    /**
+     * Relation to addresses
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function addresses() {
+        return $this->hasMany(Address::class);
+    }
+
+    /**
+     * Relation to courses
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function courses() {
+        return $this->hasMany(Course::class);
+    }
+
 }
